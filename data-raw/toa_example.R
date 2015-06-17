936ffe026ba39894e3a9d1ffa6711410b1a8e6a6
moecampos <moecampos@gmail.com>
2015-06-17 16:02:25 -0600

    Saving a first draft of the data

---
+++ data-raw/toa_example.R
@@ -0,0 +1,14 @@
+library(devtools)
+library(magrittr)
+
+toa_example <- read.csv("data-raw/toa_example.csv", stringsAsFactors = FALSE, sep = ";")
+
+toa_example$Month %<>% match(month.abb)
+toa_example$Date <- paste(toa_example$Year, toa_example$Month, toa_example$Day, sep = "-")
+toa_example$Date %<>% as.Date()
+toa_example$DayType %<>% as.factor()
+levels(toa_example$DayType) <- c("Week", "Weekend")
+toa_example$Period <- factor(toa_example$Shift, levels = c("AM", "PM"))
+toa_example$Catch <- toa_example$TotalCaught
+
+use_data(toa_example, overwrite = TRUE)
