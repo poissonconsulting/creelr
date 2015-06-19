9427467729364d36b5431788fde4efa03f0e189f
moecampos <moecampos@gmail.com>
2015-06-19 15:22:45 -0600

    Selected the specific toa_example data

--- data-raw/toa_example.R
+++ data-raw/toa_example.R
@@ -1,6 +1,7 @@
 library(devtools)
 library(magrittr)
 
+
 toa_example <- read.csv("data-raw/toa_example.csv", stringsAsFactors = FALSE, sep = ";")
 
 toa_example$Month %<>% match(month.abb)
@@ -10,5 +11,8 @@ toa_example$DayType %<>% as.factor()
 levels(toa_example$DayType) <- c("Week", "Weekend")
 toa_example$Period <- factor(toa_example$Shift, levels = c("AM", "PM"))
 toa_example$Catch <- toa_example$TotalCaught
+
+toa_example %<>% subset(select = c("Date", "DayType", "Period", "RodHours", "Catch", "KOCatch", 
+                                   "CTCatch", "RBCatch", "BTCatch", "LTCatch"))
 
 use_data(toa_example, overwrite = TRUE)
