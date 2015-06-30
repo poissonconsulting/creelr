ec7a56be5187581512c75e374dcca7ccc0d0962e
Joe Thorley <joe@poissonconsulting.ca>
2015-06-30 07:23:56 -0700

    added test for punctuate_strings

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,9 @@
+context("utils")
+
+test_that("punctuate_strings", {
+  expect_identical(punctuate_strings(c("x")), "x")
+  expect_identical(punctuate_strings(c("x","y")), "x or y")
+  expect_identical(punctuate_strings(c("x","y","z")), "x, y or z")
+  expect_identical(punctuate_strings(c("x","y","z","a")), "x, y, z or a")
+  expect_identical(punctuate_strings(c("x","y","z","a"), "and"), "x, y, z and a")
+})
