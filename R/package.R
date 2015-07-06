231a3cf6f518f0270b04777e7b0f7fcb114235d0
Joe Thorley <joe@poissonconsulting.ca>
2015-07-06 11:53:23 -0700

    release 0.0.1

--- R/package.R
+++ R/package.R
@@ -1,7 +1,10 @@
 #' Analysis of Angler Creel Data 
 #' 
 #' A prototype R package to Analyse Angler Creel Survey Data. Currently only the
-#' Traditional Access Design for One Access Site is implemented.
+#' Traditional Access Design for One Access Site method is implemented.
+#' 
+#' For a description of the available data sets type \code{data(package = "creelr")}. 
+#' For a description of the available demonstrations type \code{demo(package = "creelr")}. 
 #' 
 #' @docType package
 #' @name creelr
@@ -9,6 +12,6 @@
 #' @importFrom magrittr "%>%" "%<>%"
 #' @references Pollock, Kenneth Hugh, Cynthia M. Jones, and T. L. Brown. Angler Survey Methods and Their Applications in Fisheries Management. American Fisheries Society Special Publication 25. Bethesda, Md: American Fisheries Society, 1994.
 #' @examples
-#'  demo(toa_dummy)
-#'  demo(toa_example)
+#' demo(toa_dummy)
+#' demo(toa_example)
 NULL
